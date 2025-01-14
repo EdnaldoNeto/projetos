b933c06e63d096516daea68fe9877191e481db33
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2025-01-14 20:37:01 -0300

    update PGRAMOS 6

--- PROJETO-PGRAMOS/script.js
+++ PROJETO-PGRAMOS/script.js
@@ -1,2 +1,17 @@
-let btnScrollHorizontal = document.getElementsByClassName('btnDireita')
+let btnScrollHorizontalDireita = document.getElementsByClassName('btnDireita')
+let DivSection = document.getElementsByClassName('produtos')
+let btnScrollHorizontalEsquerda = document.getElementsByClassName('btnEsquerda')
 
+
+for(let i = 0; DivSection.length > i ; i++){
+    btnScrollHorizontalDireita[i].addEventListener('click', () => {
+        DivSection[i].scrollLeft += 680
+        btnScrollHorizontalDireita[i].style.display = 'none'
+        btnScrollHorizontalEsquerda[i].style.display = 'block'
+    })
+    btnScrollHorizontalEsquerda[i].addEventListener('click', () => {
+        DivSection[i].scrollLeft -= 680
+        btnScrollHorizontalDireita[i].style.display = 'block'
+        btnScrollHorizontalEsquerda[i].style.display = 'none'
+    })
+}
